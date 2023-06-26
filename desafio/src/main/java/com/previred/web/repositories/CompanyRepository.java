84c10f7dec3242e6184d7146b034d5f32042291f
DiegooFuentes <diegofuentesm123@gmail.com>
2023-06-26 18:00:04 -0400

    Repositories

---
+++ desafio/src/main/java/com/previred/web/repositories/CompanyRepository.java
@@ -0,0 +1,14 @@
+package com.previred.web.repositories;
+
+import com.previred.web.models.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+
+    boolean existsByRut(String rut);
+
+    void deleteByRut (String rut);
+
+}
