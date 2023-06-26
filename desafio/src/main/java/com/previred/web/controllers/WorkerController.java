43cd0044d4bd4633e8e0782214d6984002403c44
DiegooFuentes <diegofuentesm123@gmail.com>
2023-06-26 18:01:17 -0400

    Worker controller is yet to develop

---
+++ desafio/src/main/java/com/previred/web/controllers/WorkerController.java
@@ -0,0 +1,10 @@
+package com.previred.web.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/worker")
+public class WorkerController {
+
+}
