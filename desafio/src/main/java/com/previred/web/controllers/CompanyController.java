38842c573bb092f22db224cdca8233094a670b42
DiegooFuentes <diegofuentesm123@gmail.com>
2023-06-26 22:15:51 -0400

    New endpoints names

--- desafio/src/main/java/com/previred/web/controllers/CompanyController.java
+++ desafio/src/main/java/com/previred/web/controllers/CompanyController.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@RequestMapping("/company")
+@RequestMapping("/api")
 public class CompanyController {
 
     private final CompanyServiceImpl companyServiceImpl;
@@ -20,7 +20,7 @@ public class CompanyController {
         this.companyServiceImpl = companyServiceImpl;
     }
 
-    @PostMapping("/create")
+    @PostMapping("/company")
     public ResponseEntity<Object> createCompany(@RequestParam(name = "rut") String rut,
                                                 @RequestParam(name = "razonSocial") String razonSocial,
                                                 @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date) {
@@ -29,20 +29,26 @@ public class CompanyController {
 
     }
 
-    @GetMapping("/get")
+    @GetMapping("/companies")
     public List<CompanyDTO> getCompanies() {
         return companyServiceImpl.getCompanies();
     }
 
-    @PostMapping("/update")
-    public ResponseEntity<Object> updateCompany(@RequestBody Company company) {
+    @GetMapping("/company/{rut}")
+    public ResponseEntity<?> getCompany(@PathVariable String rut){
+        return companyServiceImpl.getCompany(rut);
+    }
 
-        return companyServiceImpl.updateCompany(company);
+    @PutMapping("/company/{rut}")
+    public ResponseEntity<Object> updateCompany(@PathVariable String rut,
+                                                @RequestBody Company company) {
+
+        return companyServiceImpl.updateCompany(rut, company);
 
     }
 
-    @PostMapping("/delete")
-    public ResponseEntity<Object> deleteCompany(@RequestParam(name = "rut") String rut) {
+    @DeleteMapping("/company/{rut}")
+    public ResponseEntity<Object> deleteCompany(@PathVariable String rut) {
 
         return companyServiceImpl.deleteCompany(rut);
     }
