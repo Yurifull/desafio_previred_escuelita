9ba070dc080d22fa3772142fdbf716c4b8cc41f8
DiegooFuentes <diegofuentesm123@gmail.com>
2023-06-28 11:36:43 -0400

    Swagger documentation

--- desafio/src/main/java/com/previred/web/controllers/CompanyController.java
+++ desafio/src/main/java/com/previred/web/controllers/CompanyController.java
@@ -1,9 +1,16 @@
 package com.previred.web.controllers;
 
 import com.previred.web.dtos.CompanyDTO;
-import com.previred.web.models.Company;
+import com.previred.web.dtos.WorkerDTO;
 import com.previred.web.services.CompanyServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,35 +28,158 @@ public class CompanyController {
     }
 
     @PostMapping("/company")
-    public ResponseEntity<Object> createCompany(@RequestParam(name = "rut") String rut,
-                                                @RequestParam(name = "razonSocial") String razonSocial,
-                                                @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date) {
+    @Operation(summary = "Create a company")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Company successfully created", content = @Content(examples = {
+                    @ExampleObject(name = "createCompany",
+                            summary = "Creates a company",
+                            description = "Creates a company.",
+                            value = "Company created")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE)),
+            @ApiResponse(responseCode = "409", description = "Company already exist", content = @Content(examples = {
+                    @ExampleObject(name = "companyRegistered",
+                            summary = "Shows that the company already exist",
+                            description = "Shows that the Company already exist.",
+                            value = "RUT already registered"),
+                    @ExampleObject(name = "dataError",
+                            summary = "Shows there is an error in the input data",
+                            description = "Shows there is an error in the input data.",
+                            value = "Error, verify the data")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE))})
+    public ResponseEntity<Object> createCompany(@Parameter(description = "Company's RUT (only numbers)", example = "529736541")
+                                                    @RequestParam(name = "rut") String rut,
+                                                @RequestParam(name = "companyName") String companyName,
+                                                @Parameter(description = "Creation date of the company (yyy-MM-dd)", example = "2019-02-16")
+                                                    @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date) {
 
-        return companyServiceImpl.createCompany(rut, razonSocial, date);
+        return companyServiceImpl.createCompany(rut, companyName, date);
 
     }
 
     @GetMapping("/companies")
-    public List<CompanyDTO> getCompanies() {
+    @Operation(summary = "Get a list of all companies")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Companies successfully retrieved", content = @Content(examples = {
+                    @ExampleObject(name = "retrievesCompanies",
+                            summary = "Retrieves a list of all companies",
+                            description = "Retrieves a list of all companies.",
+                            value = """
+                                    [
+                                        {
+                                            "id": 15,
+                                            "rut": null,
+                                            "companyName": null,
+                                            "date": "2021-03-07",
+                                            "workers": []
+                                        },
+                                        {
+                                            "id": 16,
+                                            "rut": "123456789",
+                                            "companyName": null,
+                                            "date": "2021-03-07",
+                                            "workers": []
+                                        }
+                                    ]""")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE))})
+    public ResponseEntity<List<CompanyDTO>> getCompanies() {
         return companyServiceImpl.getCompanies();
     }
 
     @GetMapping("/company/{rut}")
-    public ResponseEntity<?> getCompany(@PathVariable String rut){
+    @Operation(summary = "Get a company by its RUT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Company successfully retrieved", content = @Content(examples = {
+                    @ExampleObject(name = "retrievesCompany",
+                            summary = "Retrieves a company",
+                            description = "Retrieves a company.",
+                            value = """
+                                    [
+                                        {
+                                            "id": 15,
+                                            "rut": "532876493",
+                                            "companyName": "Xiaomi Chile",
+                                            "date": "2019-07-12",
+                                            "workers": []
+                                         
+                                        }
+                              
+                                    ]""")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE)),
+            @ApiResponse(responseCode = "404", description = "Company not found", content = @Content(examples = {
+                    @ExampleObject(name = "companyRegistered",
+                            summary = "Company not found",
+                            description = "Company not found.",
+                            value = "Company does not exist")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE))})
+    public ResponseEntity<?> getCompany(@Parameter(description = "Company's RUT (only numbers)", example = "529736541")
+                                            @PathVariable String rut){
         return companyServiceImpl.getCompany(rut);
     }
 
-    @PutMapping("/company/{rut}")
-    public ResponseEntity<Object> updateCompany(@PathVariable String rut,
-                                                @RequestBody Company company) {
 
-        return companyServiceImpl.updateCompany(rut, company);
+    @PutMapping("/company/{rut}")
+    @Operation(summary = "Update a company by its RUT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Company successfully updated", content = @Content(examples = {
+                    @ExampleObject(name = "updateCompany",
+                            summary = "Updates a company",
+                            description = "Updates a company.",
+                            value = "Company updated! Company{rut='503472385', companyName='Motorola', date=1994-10-13}")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE)),
+            @ApiResponse(responseCode = "404", description = "Company not found", content = @Content(examples = {
+                    @ExampleObject(name = "companyRegistered",
+                            summary = "Company not found",
+                            description = "Company not found.",
+                            value = "Company does not exist")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE))})
+    public ResponseEntity<Object> updateCompany(@Parameter(description = "Company's RUT (only numbers)", example = "529736541")
+                                                    @PathVariable String rut,
+                                                @RequestBody CompanyDTO companyDTO) {
+
+        return companyServiceImpl.updateCompany(rut, companyDTO);
 
     }
 
     @DeleteMapping("/company/{rut}")
-    public ResponseEntity<Object> deleteCompany(@PathVariable String rut) {
+    @Operation(summary = "Delete a company by its Rut")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Company successfully deleted", content = @Content(examples = {
+                    @ExampleObject(name = "createCompany",
+                            summary = "Deletes a company",
+                            description = "Deletes a company.",
+                            value = "Company deleted")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE)),
+            @ApiResponse(responseCode = "404", description = "Company not found", content = @Content(examples = {
+                    @ExampleObject(name = "companyRegistered",
+                            summary = "Company not found",
+                            description = "Company not found.",
+                            value = "Company does not exist")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE))})
+    public ResponseEntity<Object> deleteCompany(@Parameter(description = "Company's RUT (only numbers)", example = "529736541")
+                                                    @PathVariable String rut) {
 
         return companyServiceImpl.deleteCompany(rut);
+    }
+
+    @PostMapping("/worker/company")
+    @Operation(summary = "Add a worker to a company")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Worker successfully added to the company", content = @Content(examples = {
+                    @ExampleObject(name = "companyRegistered",
+                            summary = "Worker successfully added to the company",
+                            description = "Worker successfully added to the company.",
+                            value = "Worker added to Company{rut='423456789', companyName='Apple', date=2021-03-07}")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE)),
+            @ApiResponse(responseCode = "404", description = "Company not found", content = @Content(examples = {
+                    @ExampleObject(name = "companyRegistered",
+                            summary = "Company not found",
+                            description = "Company not found.",
+                            value = "Company does not exist")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE))})
+    public ResponseEntity<Object> addWorker(@Parameter(description = "Company's RUT (only numbers)", example = "529736541")
+                                                @RequestParam(name = "rut") String rut,
+                                            @RequestBody WorkerDTO workerDTO) {
+
+        return companyServiceImpl.addWorker(rut, workerDTO);
     }
 }
--- desafio/src/main/java/com/previred/web/controllers/WorkerController.java
+++ desafio/src/main/java/com/previred/web/controllers/WorkerController.java
@@ -1,10 +1,174 @@
 package com.previred.web.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.previred.web.dtos.WorkerDTO;
+import com.previred.web.services.WorkerServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/worker")
+@RequestMapping("/api")
 public class WorkerController {
+
+    private final WorkerServiceImpl workerServiceImpl;
+
+    public WorkerController(WorkerServiceImpl workerServiceImpl) {
+        this.workerServiceImpl = workerServiceImpl;
+    }
+
+    @PostMapping("/worker")
+    @Operation(summary = "Create a worker")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Worker successfully created", content = @Content(examples = {
+                    @ExampleObject(name = "createWorker",
+                            summary = "Creates a worker",
+                            description = "Creates a worker.",
+                            value = "Worker updated! Worker{rut='123456789', firstName='Fabian', lastName='Quezada', secondLastName='Mendoza', address='Gran Avenida 12'}")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE)),
+            @ApiResponse(responseCode = "409", description = "Verify data", content = @Content(examples = {
+                    @ExampleObject(name = "WorkerRegistered",
+                            summary = "Shows that worker already exist",
+                            description = "Shows that worker already exist.",
+                            value = "Worker already exist"),
+                    @ExampleObject(name = "updateWorker",
+                            summary = "Shows there is an error in the input data",
+                            description = "Shows there is an error in the input data.",
+                            value = "Error, verify the data")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE))})
+    public ResponseEntity<Object> createWorker(@Parameter(description = "Worker's RUT (only numbers)", example = "129736541")
+                                               @RequestParam(name = "rut") String rut,
+                                               @RequestParam(name = "firstName") String firstName,
+                                               @RequestParam(name = "lastName") String lastName,
+                                               @RequestParam(name = "secondLastName") String secondLastName,
+                                               @RequestParam(name = "address") String address,
+                                               @Parameter(description = "Company's RUT (only numbers)", example = "529736541")
+                                               @RequestParam(name = "companyRut", required = false) String companyRut) {
+
+        return workerServiceImpl.createWorker(rut, firstName, lastName, secondLastName, address, companyRut);
+    }
+
+    @GetMapping("/workers")
+    @Operation(summary = "Get a list of all workers")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Workers successfully retrieved", content = @Content(examples = {
+                    @ExampleObject(name = "updateWorker",
+                            summary = "Retrieves a list of workers",
+                            description = "Retrieves a list of workers.",
+                            value = """
+                                    [
+                                        {
+                                            "id": 1,
+                                            "rut": "58762341",
+                                            "firstName": "Fabian",
+                                            "lastName": "Sepulveda",
+                                            "secondLastName": "Retamal",
+                                            "address": "Arturo Prat 1423",
+                                            "company": {
+                                                "id": 18,
+                                                "rut": "548297532",
+                                                "companyName": "Apple",
+                                                "date": "2021-03-07"
+                                            }
+                                        },
+                                        {
+                                            "id": 4,
+                                            "rut": "205664112",
+                                            "firstName": "Cesar",
+                                            "lastName": "Prat",
+                                            "secondLastName": "Toloza",
+                                            "address": "Camino agricola 123",
+                                            "company": null
+                                        }]""")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE))})
+    public ResponseEntity<List<WorkerDTO>> getWorkers() {
+        return workerServiceImpl.getWorkers();
+    }
+
+    @GetMapping("/worker/{rut}")
+    @Operation(summary = "Get a worker by its RUT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Worker successfully retrieved", content = @Content(examples = {
+                    @ExampleObject(name = "retrieveWorker",
+                            summary = "Retrieves a worker",
+                            description = "Retrieves a worker.",
+                            value = """
+                                    {
+                                        "id": 6,
+                                        "rut": "198373643",
+                                        "firstName": "Camila",
+                                        "lastName": "Retamal",
+                                        "secondLastName": "Salas",
+                                        "address": "Concepción 534",
+                                        "company": {
+                                            "id": 20,
+                                            "rut": "468739543",
+                                            "companyName": "Samsung",
+                                            "date": "2003-01-02"
+                                        }
+                                    }""")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE)),
+            @ApiResponse(responseCode = "404", description = "Worker not found", content = @Content(examples = {
+                    @ExampleObject(name = "WorkerRegistered",
+                            summary = "Shows that the worker has not been found",
+                            description = "Shows that the worker has not been found.",
+                            value = "Worker does not exist")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE))})
+    public ResponseEntity<?> getWorker(@Parameter(description = "Worker's RUT (only numbers)", example = "129736541")
+                                       @PathVariable String rut) {
+        return workerServiceImpl.getWorker(rut);
+    }
+
+    @PutMapping("/worker/{rut}")
+    @Operation(summary = "Update a worker by its RUT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Worker successfully updated", content = @Content(examples = {
+                    @ExampleObject(name = "updateWorker",
+                            summary = "Updates worker information",
+                            description = "Updates worker information.",
+                            value = "Worker updated! Worker{rut='123456789', firstName='Fabian', lastName='Quezada', secondLastName='Mendoza', address='Gran Avenida 12'}")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE)),
+            @ApiResponse(responseCode = "404", description = "Worker not found", content = @Content(examples = {
+                    @ExampleObject(name = "workerNotUpdated",
+                            summary = "Shows that worker does not exist",
+                            description = "Shows that worker does not exist.",
+                            value = "Worker does not exist")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE))})
+    public ResponseEntity<Object> updateWorker(@Parameter(description = "Worker's RUT (only numbers)", example = "129736541")
+                                               @PathVariable(name = "rut") String rut,
+                                               @Parameter(description = "Company's RUT (only numbers ex.529736541)")
+                                               @RequestParam(name = "companyRut", required = false) String companyRut,
+                                               @RequestBody WorkerDTO workerDTO) {
+
+        return workerServiceImpl.updateWorker(rut, workerDTO, companyRut);
+    }
+
+    @DeleteMapping("/worker/{rut}")
+    @Operation(summary = "Delete a worker by its RUT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Worker successfully deleted", content = @Content(examples = {
+                    @ExampleObject(name = "deleteWorker",
+                            summary = "Deletes a worker",
+                            description = "Deletes a worker.",
+                            value = "Worker deleted")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE)),
+            @ApiResponse(responseCode = "404", description = "Worker not found", content = @Content(examples = {
+                    @ExampleObject(name = "workerNotDeleted",
+                            summary = "Shows that worker does not exist",
+                            description = "Shows that worker does not exist.",
+                            value = "Worker does not exist")
+            }, mediaType = MediaType.APPLICATION_JSON_VALUE))})
+    public ResponseEntity<Object> deleteWorker(@PathVariable String rut) {
+
+        return workerServiceImpl.deleteWorker(rut);
+    }
+
 
 }
